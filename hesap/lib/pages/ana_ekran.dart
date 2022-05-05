21777795918354096316e2743c66e2488e965a5c
M.Zeynep Yavuz <melikezeynep_97@hotmail.com>
2022-05-06 00:43:09 +0300

    SliverAppBar taslağı oluşturuldu.
    (Overflow hatası henüz çözülmedi)

--- hesap/lib/pages/ana_ekran.dart
+++ hesap/lib/pages/ana_ekran.dart
@@ -1,10 +1,12 @@
 // necessary
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 // components
 import 'package:hesap/components/elements/hesap_floating_action_button.dart';
 import 'package:hesap/components/screen_sections/hesap_bottom_navigation_bar.dart';
 import 'package:hesap/pages/qr_okuma_ekran.dart';
+import 'package:hesap/theme/colors.dart';
 
 class AnaSayfa extends StatefulWidget {
   const AnaSayfa({Key? key}) : super(key: key);
@@ -17,7 +19,26 @@ class _AnaSayfaState extends State<AnaSayfa> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: CustomScrollView(
+        slivers: [
+          SliverPersistentHeader(
+            delegate: SliverAppBar(),
+            pinned: true,
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+              return const Card(
+                child: Text(
+                  'Lorem Ipsum Kafe',
+                  style: TextStyle(fontSize: 30),
+                ),
+              );
+            },
+            childCount: 20,
+            )
+          )
+        ],
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: HesapFloatingActionButton(
           icon: Icons.qr_code_scanner,
@@ -28,4 +49,131 @@ class _AnaSayfaState extends State<AnaSayfa> {
       bottomNavigationBar: const HesapBottomNavigationBar(),
     );
   }
+}
+
+class SliverAppBar extends SliverPersistentHeaderDelegate {
+  SliverAppBar();
+  double usttaraf = 20;
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(
+          height: usttaraf,
+        ),
+        Stack(
+          alignment: AlignmentDirectional.topEnd,
+          children: [
+            Transform.scale(
+              scale: 1.05,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(70),
+                  bottomRight: Radius.circular(70),
+                ),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height:200,
+                  color: AppColors.primary,
+                ),
+              ),
+            ),
+            SvgPicture.asset('assets/images/background.svg'),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: const Padding(
+                padding: EdgeInsets.all(18.0),
+                child: Text(
+                  'hesap',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 210,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(
+                    Icons.location_on_outlined,
+                    color: Colors.white,
+                    size: 25,
+                  ),
+                  Text(
+                    ' Konum',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 240,
+              child: Align(
+                alignment: AlignmentDirectional.bottomCenter,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width*0.82,
+                    height: 80,
+                    color: Colors.white,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(
+                          child: Container(
+                            margin: const EdgeInsets.all(18),
+                            alignment: Alignment.center,
+                            child: const Text(
+                              'Menüyü görüntülemek için QR kodu okutun',
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.qr_code,
+                            size: 60,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  @override
+  double get maxExtent => 240 + usttaraf;
+
+  @override
+  double get minExtent => 20;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      oldDelegate.maxExtent != maxExtent || oldDelegate.minExtent != minExtent;
 }
